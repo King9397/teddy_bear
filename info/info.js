80d9af4b34cd1a989da591a1fe0f085f9fff06f9
yunyeonjeong <duswjddl0817@naver.com>
2023-07-06 17:05:35 +0900

    230706 1705 main 수정

--- info/info.js
+++ info/info.js
@@ -1,32 +1,18 @@
-// const 내가정한식별자명 = new Swiper('대상', { 객체(옵션) });
-const mySwiper1 = new Swiper('.swiper_wrapper', {
-  // direction: 'vertical', 세로 방향 슬라이드
-  loop: true, // 무한 반복
+// 지점 안내 자동 슬라이더 및 버튼 슬라이더
 
-  pagination: {
-    // 하단 페이지네이션
-    el: '.swiper-pagination',
-    clickable: true,
-    dynamicBullets: true,
-  },
-
-  navigation: {
-    // 좌 우 화살표
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
-
-const mySwiper2 = new Swiper('.mySwiper2', {
-  loop: true,
-  pagination: {
-    // 하단 페이지네이션
-    el: '.mySwiper2 .swiper-pagination',
-    clickable: true,
-  },
+const mySwiper = new Swiper('.mySwiper', {
+  spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
