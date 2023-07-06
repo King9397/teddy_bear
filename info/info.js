a492b1e2a275df786ea5879aa1db0f1d859684bd
yunyeonjeong <duswjddl0817@naver.com>
2023-07-06 17:55:58 +0900

    230706 1755 수정

--- info/info.js
+++ info/info.js
@@ -4,12 +4,8 @@ const mySwiper = new Swiper('.mySwiper', {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
-    delay: 2500,
+    delay: 3000,
     disableOnInteraction: false,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
   },
   navigation: {
     nextEl: '.swiper-button-next',
