319e0b7dc13fe8f343072615f54611d63aaac435
yunyeonjeong <duswjddl0817@naver.com>
2023-07-07 14:34:03 +0900

    230707 1433 수정

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-window.addEventListener('load', function() {
-    var ear1 = document.querySelector('.welcome .container .ear1');
-    var face = document.querySelector('.welcome .container .face');
-    var ear2 = document.querySelector('.welcome .container .ear2');
-  
-    // 이미지 요소들을 모이는 값
-    function moveImagesToCenter() {
-      ear1.classList.add('move');
-      face.classList.add('move');
-      ear2.classList.add('move');
+window.addEventListener('load', function () {
+  var ear1 = document.querySelector('.welcome .container .ear1');
+  var face = document.querySelector('.welcome .container .face');
+  var ear2 = document.querySelector('.welcome .container .ear2');
+
+  // 이미지 요소들을 모이는 값
+  function moveImagesToCenter() {
+    ear1.classList.add('move');
+    face.classList.add('move');
+    ear2.classList.add('move');
+  }
+
+  setTimeout(moveImagesToCenter, 100);
+});
+
+// 다섯 번째 섹션(TeddyBear 페이지) 스크롤 시 폰트 색상 변경
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 1) {
+      $('.text-moving');
+    } else {
+      $('.text-moving').css('color', '#343a40');
     }
-    
-    setTimeout(moveImagesToCenter, 100);
-
-
+  });
 });
