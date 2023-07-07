de0f9ec039e7efc3c5d7d3b77098dfa7ef40fdbf
mellow9307 <mellow9307@gmail.com>
2023-07-07 12:16:56 +0900

    menu수정중

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-window.addEventListener('load', function() {
-    var ear1 = document.querySelector('.welcome .container .ear1');
-    var face = document.querySelector('.welcome .container .face');
-    var ear2 = document.querySelector('.welcome .container .ear2');
-  
-    // 이미지 요소들을 모이는 값
-    function moveImagesToCenter() {
-      ear1.classList.add('move');
-      face.classList.add('move');
-      ear2.classList.add('move');
-    }
-    
-    setTimeout(moveImagesToCenter, 100);
+window.addEventListener("load", function () {
+  var ear1 = document.querySelector(".welcome .container .ear1");
+  var face = document.querySelector(".welcome .container .face");
+  var ear2 = document.querySelector(".welcome .container .ear2");
 
+  // 이미지 요소들을 모이는 값
+  function moveImagesToCenter() {
+    ear1.classList.add("move");
+    face.classList.add("move");
+    ear2.classList.add("move");
+  }
 
+  setTimeout(moveImagesToCenter, 100);
+});
+
+// menu btn
+$(document).ready(function () {
+  $(".menu_wrapper_nav a").on("click", (e) => {
+    e.preventDefault();
+    $(".menu_wrapper_list").slideToggle();
+    $(".menu_wrapper_list").toggleClass("hide");
+  });
 });
