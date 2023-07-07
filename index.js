fc3fcedf07139ad5a1e64d8a45f4b243b8222dde
RyuKyoungHyun <soccergh@naver.com>
2023-07-07 15:14:16 +0900

    230707 index 곰돌이 슬라이드 수정

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
+window.addEventListener('load', function () {
+  var ear1 = document.querySelector('.welcome .container .ear1');
+  var face = document.querySelector('.welcome .container .face');
+  var ear2 = document.querySelector('.welcome .container .ear2');
 
-window.addEventListener('load', function() {
-    var ear1 = document.querySelector('.welcome .container .ear1');
-    var face = document.querySelector('.welcome .container .face');
-    var ear2 = document.querySelector('.welcome .container .ear2');
-  
-    // 이미지 요소들을 모이는 값
-    function moveImagesToCenter() {
-      ear1.classList.add('move');
-      face.classList.add('move');
-      ear2.classList.add('move');
-    }
-    
+  // 이미지 요소들을 모이는 값
+  function moveImagesToCenter() {
+    ear1.classList.add('move');
+    face.classList.add('move');
+    ear2.classList.add('move');
+  }
+
   setTimeout(moveImagesToCenter, 100);
 });
-   var swiper = new Swiper(".mySwiper", {
-      navigation: {
-      nextEl: ".swiper-next",
-      prevEl: ".swiper-prev",
-     },
+var swiper = new Swiper('.mySwiper', {
+  // direction: 'vertical',
+  // loop: true,
 });
-
