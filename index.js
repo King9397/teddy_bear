e215bb237ad71d0f08a12a848ed1378974efd9c7
yunyeonjeong <duswjddl0817@naver.com>
2023-07-07 16:49:57 +0900

    230707 1649 수정

--- index.js
+++ index.js
@@ -1,51 +1,26 @@
-<<<<<<< HEAD
-window.addEventListener('load', function () {
-  var ear1 = document.querySelector('.welcome .container .ear1');
-  var face = document.querySelector('.welcome .container .face');
-  var ear2 = document.querySelector('.welcome .container .ear2');
+// 첫번째 섹션 곰돌이
+window.addEventListener("load", function () {
+  var ear1 = document.querySelector(".welcome .container .ear1");
+  var face = document.querySelector(".welcome .container .face");
+  var ear2 = document.querySelector(".welcome .container .ear2");
 
   // 이미지 요소들을 모이는 값
   function moveImagesToCenter() {
-    ear1.classList.add('move');
-    face.classList.add('move');
-    ear2.classList.add('move');
+    ear1.classList.add("move");
+    face.classList.add("move");
+    ear2.classList.add("move");
   }
 
   setTimeout(moveImagesToCenter, 100);
 });
 
-// 다섯 번째 섹션(TeddyBear 페이지) 스크롤 시 폰트 색상 변경
-$(document).ready(function () {
-  $(window).scroll(function () {
-    var scroll = $(window).scrollTop();
-    if (scroll > 1) {
-      $('.text-moving');
-    } else {
-      $('.text-moving').css('color', '#343a40');
-    }
-  });
+//세번째 섹션 자동 슬라이더
+const mySwiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  // centeredSlides: true,
+  slidesPerView: "1.5",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
-=======
-
-window.addEventListener('load', function() {
-    var ear1 = document.querySelector('.welcome .container .ear1');
-    var face = document.querySelector('.welcome .container .face');
-    var ear2 = document.querySelector('.welcome .container .ear2');
-  
-    // 이미지 요소들을 모이는 값
-    function moveImagesToCenter() {
-      ear1.classList.add('move');
-      face.classList.add('move');
-      ear2.classList.add('move');
-    }
-    
-  setTimeout(moveImagesToCenter, 100);
-});
-   var swiper = new Swiper(".mySwiper", {
-      navigation: {
-      nextEl: ".swiper-next",
-      prevEl: ".swiper-prev",
-     },
-});
-
->>>>>>> 0508e69b0d423cae7180fd87c7d503c59c86bdb3
--- info/info.js
+++ info/info.js
@@ -1,14 +1,14 @@
 // 지점 안내 자동 슬라이더 및 버튼 슬라이더
 
-const mySwiper = new Swiper('.mySwiper', {
+const mySwiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
-    delay: 3000,
+    delay: 2000,
     disableOnInteraction: false,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
