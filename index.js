9aaae2cd64103c5ece2dfee8f003538d4cbc102b
mellow9307 <mellow9307@gmail.com>
2023-07-10 09:34:21 +0900

    깃페이지 연결

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ window.addEventListener("load", function () {
 });
 
 const bearLoading = function () {
-      var ear1 = document.querySelector(".welcome .container .ear1");
-      var face = document.querySelector(".welcome .container .face");
-      var ear2 = document.querySelector(".welcome .container .ear2");
-      ear1.classList.add("move");
-      face.classList.add("move");
-      ear2.classList.add("move");
-}
+  var ear1 = document.querySelector(".welcome .container .ear1");
+  var face = document.querySelector(".welcome .container .face");
+  var ear2 = document.querySelector(".welcome .container .ear2");
+  ear1.classList.add("move");
+  face.classList.add("move");
+  ear2.classList.add("move");
+};
 
 setInterval(bearLoading, 3000);
 
@@ -28,16 +28,16 @@ const bearRemove = function () {
   var ear1 = document.querySelector(".welcome .container .ear1");
   var face = document.querySelector(".welcome .container .face");
   var ear2 = document.querySelector(".welcome .container .ear2");
-  console.log('ddd')
+  console.log("ddd");
   ear1.classList.remove("move");
   face.classList.remove("move");
   ear2.classList.remove("move");
-}
+};
 
 setInterval(bearRemove, 6000);
 //세번째 섹션 자동 슬라이더
 const mySwiper = new Swiper(".mySwiper", {
-  spaceBetween: -180,
+  spaceBetween: -160,
   slidesPerView: "1.8",
   slidesOffsetBefore: 180,
   slidesOffsetAfter: -100,
@@ -110,53 +110,54 @@ $(window).on("scroll", () => {
 });
 
 //  이중 스크롤 //:c 이중스크롤 수정입니다
-$(window).on('scroll', () => {
-  const aniTop = document.querySelector('.animation').offsetTop;
-  const whereTop = document.querySelector('.animation .where_wrapper').offsetTop;
-  const aniHt = document.querySelector('.animation').clientHeight
-  const whereHt = document.querySelector('.animation .where').clientHeight;
-  console.log(scrollY)
-  console.log(scrollY + aniHt - whereHt)
+$(window).on("scroll", () => {
+  const aniTop = document.querySelector(".animation").offsetTop;
+  const whereTop = document.querySelector(
+    ".animation .where_wrapper"
+  ).offsetTop;
+  const aniHt = document.querySelector(".animation").clientHeight;
+  const whereHt = document.querySelector(".animation .where").clientHeight;
+  console.log(scrollY);
+  console.log(scrollY + aniHt - whereHt);
   if (scrollY >= aniTop && scrollY <= aniTop + aniHt - whereHt) {
     $(".animation .scroll_bar").css({
       transform: `translateY(${scrollY - aniTop + 120}px)`,
     });
   }
 
-  $('.scroll_bar .scroll_bar_black').css({
-    height: `${(scrollY - aniTop) / (aniHt - whereHt)  * 100}%`,
-    maxHeight: `100%`
-  })
+  $(".scroll_bar .scroll_bar_black").css({
+    height: `${((scrollY - aniTop) / (aniHt - whereHt)) * 100}%`,
+    maxHeight: `100%`,
+  });
 
   if (scrollY >= aniTop) {
-    $('.zookeeper h2').css({
-      opacity : 1,
-      transform: `translateY(0)`
-    })
-  } else {
-    $('.zookeeper h2').css({
-      opacity : 0,
-      transform: `translateY(100px)`,
-    })
-  }
-  if (scrollY >= (aniTop + whereTop)) {
-    $('.where p').css({
-      opacity : 1,
+    $(".zookeeper h2").css({
+      opacity: 1,
       transform: `translateY(0)`,
-    })
-    $('.where .btn').css({
-      opacity : 1,
-      transform: `translateY(0)`,
-    })
+    });
   } else {
-    $('.where p').css({
-      opacity : 0,
+    $(".zookeeper h2").css({
+      opacity: 0,
       transform: `translateY(100px)`,
-    })
-    $('.where .btn').css({
-      opacity : 0,
-      transform: `translateY(100px)`,
-    })
+    });
   }
-
-})
+  if (scrollY >= aniTop + whereTop) {
+    $(".where p").css({
+      opacity: 1,
+      transform: `translateY(0)`,
+    });
+    $(".where .btn").css({
+      opacity: 1,
+      transform: `translateY(0)`,
+    });
+  } else {
+    $(".where p").css({
+      opacity: 0,
+      transform: `translateY(100px)`,
+    });
+    $(".where .btn").css({
+      opacity: 0,
+      transform: `translateY(100px)`,
+    });
+  }
+});
