b09ecfca72ade45fff9517c7934c4f6e096890d0
yunyeonjeong <duswjddl0817@naver.com>
2023-07-10 17:27:33 +0900

    230710 1727 헤더 및 푸더, 인포 수정

--- info/info.js
+++ info/info.js
@@ -1,21 +1,20 @@
 // 지점 안내 자동 슬라이더 및 버튼 슬라이더
-
-const mySwiper = new Swiper(".mySwiper", {
+const mySwiper = new Swiper('.mySwiper', {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
-    delay: 2000,
+    delay: 3000,
     disableOnInteraction: false,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
 
 // 이용안내 svg
-const arrow_left = document.querySelectorAll(".charge_wrapper svg path");
-$(window).on("scroll", () => {
+const arrow_left = document.querySelectorAll('.charge_wrapper svg path');
+$(window).on('scroll', () => {
   arrow_left.forEach(function (a, idx) {
     let length = a.getTotalLength();
     // console.log(length);
